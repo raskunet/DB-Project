277ab5cf9e91c3099fcd880326cf2218ce09b819
Hamza Sajjad <hamzasajjad338@gmail.com>
2024-05-05 23:58:38 +0500

    working category thing

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -4,15 +4,19 @@ exports.renderShopProduct = async function (req, res, next) {
     try {
         // Query the database to fetch products
         const pool = await sqlCon;
-        const result = await pool.request().query('SELECT * FROM Products');
+        const productsResult = await pool.request().query('SELECT * FROM Products');
 
-        // Extract products from the result
-        const products = result.recordset;
+        // Query the database to fetch categories
+        const categoriesResult = await pool.request().query('SELECT * FROM Category');
 
-        // Render the shopProduct page and pass products data to the template
-        res.render('shopProduct', { products });
+        // Extract products and categories from the results
+        const products = productsResult.recordset;
+        const categories = categoriesResult.recordset;
+
+        // Render the shopProduct page and pass products and categories data to the template
+        res.render('shopProduct', { products, categories });
     } catch (error) {
-        console.error("Error fetching products:", error);
+        console.error("Error fetching products or categories:", error);
         // Handle errors appropriately
         res.status(500).send('Internal Server Error');
     }
