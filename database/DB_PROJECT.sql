f40846d3de7490415b52b896e4a9e88ef807bc12
raskunet <111647493+raskunet@users.noreply.github.com>
2024-05-06 16:40:38 +0500

    Modified Table Schema

--- database/DB_PROJECT.sql
+++ database/DB_PROJECT.sql
@@ -28,12 +28,15 @@ create table Category(
     categoryName nvarchar(30)
 );
 
+drop table Products
 CREATE TABLE Products (
     productID int primary key IDENTITY (1,1),
     productName nvarchar(100) not null,
     price decimal(10, 2) not null,
     description nvarchar(255),
-    imagepath nvarchar(100) not null
+    imagepath nvarchar(100) not null,
+    categoryID int NOT NULL,
+    FOREIGN KEY (categoryID) references Category(categoryID),
 );
 
 
@@ -99,42 +102,42 @@ INSERT INTO Category (categoryName) VALUES ('Peripherals');
 
 
 -- Insert product 1
-INSERT INTO Products (productName, price, description, imagepath,categoryId)
+INSERT INTO Products (productName, price, description, imagepath, categoryID)
 VALUES ('Ultra Gamer''s Delight', 999.99, 'Experience the pinnacle of gaming performance...', '/images/products/PC.jpg',1);
 
 -- Insert product 2
-INSERT INTO Products (productName, price, description, imagepath,categoryId)
+INSERT INTO Products (productName, price, description, imagepath, categoryID)
 VALUES ('Ergo-Elite Gaming Chair', 699.99, 'Elevate your gaming experience with the ultimate comfort and support of the Secretlab gaming chair.', '/images/products/gamingchair.jpeg',2);
 
 -- Insert product 3
-INSERT INTO Products (productName, price, description, imagepath,categoryId)
+INSERT INTO Products (productName, price, description, imagepath, categoryID)
 VALUES ('HyperStrike Mechanical Keyboard', 199.99, 'Dominate every keystroke with the precision and speed of the ASUS KJoy fully mechanical keyboard with Cherry switches.', '/images/products/keyboard.jpg',2);
 
 -- Insert product 4
-INSERT INTO Products (productName, price, description, imagepath,categoryId)
+INSERT INTO Products (productName, price, description, imagepath, categoryID)
 VALUES ('ROG Strix Supreme Motherboard', 699.99, 'Unleash the power of the latest technology with the cutting-edge ROG motherboard, designed for ultimate performance.', '/images/products/mobo.jpg',2);
 
 -- Insert product 5
-INSERT INTO Products (productName, price, description, imagepath,categoryId)
+INSERT INTO Products (productName, price, description, imagepath, categoryID)
 VALUES ('Titanic Performance Pack', 1299.99, 'Conquer any task with ease using the TREX Pack featuring an i9 13th gen processor and a massive 2TB SSD storage.', '/images/products/pcpack.jpeg',1);
 
 -- Insert product 6
-INSERT INTO Products (productName, price, description, imagepath,categoryId)
+INSERT INTO Products (productName, price, description, imagepath, categoryID)
 VALUES ('Titanium Edition RTX 4070TI', 899.99, 'Immerse yourself in breathtaking visuals with the ASUS RTX 4070TI, delivering unparalleled graphics performance.', '/images/products/gpu.jpg',2);
 
 -- Insert product 7
-INSERT INTO Products (productName, price, description, imagepath,categoryId)
+INSERT INTO Products (productName, price, description, imagepath, categoryID)
 VALUES ('Silent Fury Gaming Mouse', 699.99, 'Achieve precision and silence with the Razor K25 gaming mouse, designed for competitive gaming with whisper-quiet clicks.', '/images/products/mouse.jpeg',2);
 
 -- Insert product 8
-INSERT INTO Products (productName, price, description, imagepath,categoryId)
+INSERT INTO Products (productName, price, description, imagepath, categoryID)
 VALUES ('Neon Horizon RGB Case', 699.99, 'Showcase your build in style with the FNF Case featuring vibrant RGB lighting and superior airflow for optimal performance.', '/images/products/case.jpg',2);
 
 -- Insert product 9
-INSERT INTO Products (productName, price, description, imagepath,categoryId)
+INSERT INTO Products (productName, price, description, imagepath, categoryID)
 VALUES ('Ultimate Dual Monitor Setup', 1999.99, 'Transform your workspace into a productivity powerhouse with the Razor Pack, including dual 4K monitors and an i9 13th gen processor.', '/images/products/pcpack3.jpg',1);
 
 -- Insert product 10
-INSERT INTO Products (productName, price, description, imagepath,categoryId)
+INSERT INTO Products (productName, price, description, imagepath, categoryID)
 VALUES ('Mystic Beast PC', 699.99, 'Unleash the power of the Mystic Beast, featuring top-of-the-line components for unbeatable performance in gaming and productivity tasks.', '/images/products/pcpack4.jpeg',1);
 select *from products
